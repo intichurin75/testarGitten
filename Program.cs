5ea0f107c720af05610af6bd0709347578f908d8
intichurin75 <146718879+intichurin75@users.noreply.github.com>
2023-10-28 08:56:48 +0200

    Update Program.cs
    Har lagt till ett räkne exempel.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ class Program
     {
         Console.WriteLine("Hello,nu har jag lagt in en förändring i koden!");
         Console.WriteLine("Lagt in ytterligare kommentar");
+
+        int nr1 = 12;
+        int nr2 = 5;
+        int summa = nr + nr2;
+        Console.WriteLine($"Summan är : {summa})
         
     }
 }
